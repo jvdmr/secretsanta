243d9c4655439acff2e22761797f098ce7d239d0
Joris Vandermeersch <joris.vandermeersch@roots.be>
2017-12-13 10:05:50 +0100

    recreating functions

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,44 @@
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.secretSanta = functions.https.onRequest((req, res) => {
-	if (req.method == 'POST') {
-		res.send(req.body);
-	} else {
-		res.writeHead(404);
-		res.end('Not Found');
+exports.echo = functions.https.onRequest(post((req, res) => {
+	res.send(req.body);
+}));
+
+exports.secretSanta = functions.https.onRequest(post((req, res) => {
+	res.send(req.body);
+}));
+
+exports.mail = functions.https.onRequest(post((req, res) => {
+	const data = req.body;
+	const mailOptions = {
+		from: '"SecretSanta" <noreply@secretsanta.vdmr.be>',
+		to: data.email,
+		subject: "Test",
+		html: "ikel<br/>hahaha"
+	};
+	mailTransport.sendMail(mailOptions);
+}));
+
+const mailTransport = nodemailer.createTransport({
+	service: 'Gmail',
+	auth: {
+		user: functions.config().gmail.email,
+		pass: functions.config().gmail.password
 	}
 });
+
+
+function post(f) {
+	return (req, res) => {
+		if (req.method == 'POST') {
+			f(req, res);
+		} else {
+			res.writeHead(404);
+			res.end('Not Found');
+		}
+	}
+}
